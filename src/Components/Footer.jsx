3091c977abd836cb57f60d6736600df6643f3d47
Puneet K Jattana <puneetkaurjattana@gmail.com>
2020-10-27 17:35:28 +0530

    competed notes app

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,6 +1,12 @@
 import React from "react";
-function Footer(){
-    var d=new Date(); var year=d.getFullYear();
-return <footer><p>Copyright ©{year}</p></footer>
+
+function Footer() {
+  const year = new Date().getFullYear();
+  return (
+    <footer>
+      <p>Copyright ⓒ {year}</p>
+    </footer>
+  );
 }
+
 export default Footer;
--- src/Components/Note.jsx
+++ src/Components/Note.jsx
@@ -1,15 +1,16 @@
 import React from "react";
-import Notes from "../notes"
-function Note() {
-  return (Notes.map((note) => {
-return (<div className="note">
-<h1>{note.title}</h1>
-      <p>{note.content}</p>
-    </div>);
+
+function Note(props) {
+  function handleClick() {
+    props.onDelete(props.id);
   }
-  )
-    
-     
+
+  return (
+    <div className="note">
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
+      <button onClick={handleClick}>DELETE</button>
+    </div>
   );
 }
 
